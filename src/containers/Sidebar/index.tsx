e2f2041e8468c1962e0b951723b1d2e8e8e9d11c
MSixels <msixel22@gmail.com>
2023-09-22 16:21:55 -0300

    Estilizando a SideBar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/MSixels.png" />
-    <Titulo fontSize={20}>Matheus Sixel</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Matheus Sixel</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        MSixelS
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 export default SideBar
